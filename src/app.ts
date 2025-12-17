20ead6cd144e3ebfa87ce55406060fffae9a3bf4
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2025-12-17 13:50:07 +0700

    re routing app

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,6 @@ import todoRoutes from "./routes/todo.route.js";
 const app = express();
 
 app.use(express.json());
-app.use("/todos", todoRoutes);
+app.use("/api/todos", todoRoutes);
 
 export default app;
