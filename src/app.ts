2fd587ff27860605e5a66f7be5e6a52c20c04447
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2026-01-05 17:42:44 +0700

    feat: setup cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
 import express from "express";
+import cors from "cors";
 import todoRoutes from "./routes/todo.route";
 
 const app = express();
 const PORT = 3000;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use("/todos", todoRoutes);
