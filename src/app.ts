8c6764e45aefab4eb4e86ccc74e313f40118cdbb
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2026-01-05 17:30:16 +0700

    feat: add listening port

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,13 @@ import express from "express";
 import todoRoutes from "./routes/todo.route";
 
 const app = express();
+const PORT = 3000;
 
 app.use(express.json());
 app.use("/todos", todoRoutes);
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
 
 export default app;
