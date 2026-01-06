3bba48a000d179c1b59fa7bf6d66c5e009b818a8
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2026-01-06 10:28:03 +0700

    add another localhost pwa

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const PORT = 3000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://localhost:4173"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   })
